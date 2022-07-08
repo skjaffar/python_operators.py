d49d722121219a56e673b59c6d4cae100b789629
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-07 22:27:34 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,31 @@
+# # '''' Arithmetic operators ''''
+
+# a=float(input("enter the value of a:"))
+# b=float(input("enter the value of b:"))
+# a,b=[int(x) for x in input('enter the value of a and b: ')]
+
+# print(f"The value of {a} + {b} is {a+b}")
+# print(f"The value of {a} - {b} is {a-b}")
+# print(f"The value of {a} * {b} is {a*b}")
+# print(f"The value of {a} / {b} is {a/b}")
+# print(f"The value of {a} % {b} is {a%b}")
+# print(f"the value of {a} ** {b} is {a**b}")
+
+# ''''floor division'''
+
+# d=9//6
+# print(d)
+# f=6//3.0
+# print(f)
+# o=8//3
+# print(o)
+# i=8//3.0
+# print(i)
+
+'''' Assignment operators '''
+
+a=8 ; b=9
+a=a+b
+print(a)
+a=b+b
+print(a)
